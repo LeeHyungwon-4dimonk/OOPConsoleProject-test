cf7e838d5a606ee7ecb1829aab1ae54a295582a9
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-04-07 11:27:24 +0900

    [feat] Game Scene Class Framework feat
    게임 클래스를 구현하고 여러 씬들을 가질 수 있도록 구현하여, 씬들을 추가하여 게임 내용을 구현할 수 있도록 만듬

---
+++ OOPConsoleProject/OOPConsoleProject/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPConsoleProject.Scenes;
+
+namespace OOPConsoleProject
+{
+    public static class Game
+    {
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curscene;
+
+        private static bool gameOver;
+
+        
+        public static void Run()
+        {
+            Start();
+
+            while(gameOver == false)
+            {
+                Console.Clear();
+                curscene.Render();
+                curscene.Input();
+                curscene.Update();
+                curscene.Result();
+            }
+
+            End();
+        }
+        /// <summary>
+        /// 게임의 초기 설정을 진행
+        /// </summary>
+        private static void Start()
+        {
+            // 게임 설정
+            gameOver = false;
+
+            // 씬 설정
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curscene = sceneDic["Title"];
+        }
+        /// <summary>
+        /// 게임의 마무리 작업 진행
+        /// </summary>
+        private static void End()
+        {
+
+        }
+    }
+}
---
+++ OOPConsoleProject/OOPConsoleProject/Scenes/TitleScene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class TitleScene : BaseScene
+    {   
+        public override void Render()
+        {
+            Console.WriteLine("**********************");
+            Console.WriteLine("*     레전드 RPG     *");
+            Console.WriteLine("**********************");
+            Console.WriteLine();
+            Console.WriteLine("계속 하려면 아무 키나 누르세요...");
+        }
+
+        public override void Input()
+        {
+            Console.ReadKey(true);
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void Result()
+        {
+
+        }
+
+        
+    }
+}
